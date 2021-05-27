ba0b4bc985e0ed84ad02e66f87d748e759390ec9
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-05-27 15:28:04 -0400

    Added reactions from GitHub API to the issues component

--- src/components/issues/issues.component.jsx
+++ src/components/issues/issues.component.jsx
@@ -7,6 +7,13 @@ const Issues = ({ repository }) => (
                     <a href={issue.node.url}>
                         {issue.node.title}
                     </a>
+                    <ul>
+                        {issue.node.reactions.edges.map(reaction => (
+                            <li key={reaction.node.id}>
+                                {reaction.node.content}
+                            </li>
+                        ))}
+                    </ul>
                 </li>
             ))
         }
